9bb2c00343d0c8f8b5e209b7ba0c4e8b57eee309
Stas Raranetskyi <rar.stas@gmail.com>
2023-01-19 22:48:19 +0000

    import svg like react component

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
 	reactStrictMode: true,
 	basePath: '',
 	i18n,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
 };
 
 module.exports = nextConfig;
